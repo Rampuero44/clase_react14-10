f0fbb60220c955ceb5624ad9d7a747ffe75d1572
PAAWSA02LC0900 <Duoc@PAAWSA02LC0900.duoc.acad>
2025-10-07 22:22:39 -0300

    Avances programacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Titulo from './components/titulo';
+import Banner from './components/banner';
+import { useState } from 'react';
 
 function App() {
+  const [sueldoBruto, setSueldoBruto] = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <Titulo/>
+        <Banner/>
+
+        {/* Seccion de calculo de remuneracion */}
+
+        <div className='row mt-3'>
+          <div className='col-lg-6'>
+            <h4>Datos sueldo</h4>
+            <label className='form-label' htmlFor='sueldoBruto'>Ingrese sueldo bruto</label>
+            <input id='sueldoBruto' name='sueldoBruto' placeholder='1000000' type='text' className='form-control' value={sueldoBruto} onChange={(e) => setSueldoBruto(e.target.value)}></input>
+          </div>
+
+          <div className='col-lg-6'>
+            <h4>Resultados</h4>
+            <p>Sueldo bruto: {sueldoBruto}</p>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/titulo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function tituloPrincipal() {
+    console.log('Componente montado');
+
+    return (
+        <div className='row mt-3'>
+          <div className='col-lg-12'>
+            <h2>Pagina de Recursos humanos</h2>
+            <p>Pagina de empleados de DUOC UC</p>
+          </div>
+        </div>
+    );
+}
+
+export default tituloPrincipal;
